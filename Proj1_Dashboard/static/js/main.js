d7cdf1b56d599b9851cb69de201dc16ff10c7111
sh95fit <h95fit@gmail.com>
2023-02-07 13:58:25 +0900

    대시보드 - 네비게이션바 구성

--- Proj1_Dashboard/static/js/main.js
+++ Proj1_Dashboard/static/js/main.js
@@ -1,3 +1,5 @@
+
+//Sidebar Dropdown
 const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
 
 allDropdown.forEach(item=> {
@@ -18,4 +20,25 @@ allDropdown.forEach(item=> {
         this.classList.toggle('active');
         item.classList.toggle('show');
     })
+})
+
+
+//Profile Dropdown
+const profile = document.querySelector('nav .profile');
+const imgProfile = profile.querySelector('img');
+const dropdownProfile = profile.querySelector('.profile-link');
+
+imgProfile.addEventListener('click', function() {
+    dropdownProfile.classList.toggle('show');
+})
+
+
+window.addEventListener('click', function(e) {
+    if(e.target !== imgProfile) {
+        if(e.target !== dropdownProfile) {
+            if(dropdownProfile.classList.contains('show')){
+                dropdownProfile.classList.remove('show');
+            }
+        }
+    }
 })
