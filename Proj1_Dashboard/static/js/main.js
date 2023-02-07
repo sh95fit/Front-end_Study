f5b27e6bae651be3f251f1cdaf9a88f14731b20f
sh95fit <h95fit@gmail.com>
2023-02-07 11:03:11 +0900

    대시보드 - 사이드바 구성

---
+++ Proj1_Dashboard/static/js/main.js
@@ -0,0 +1,21 @@
+const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
+
+allDropdown.forEach(item=> {
+    const a = item.parentElement.querySelector('a:first-child');
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // 토글 형태의 버튼이 하나만 active 상태가 되도록 만들기(기존 것 remove 후 선택한 것 active)
+        if(!this.classList.contains('active')) {
+            allDropdown.forEach(i=> {
+                const aLink = i.parentElement.querySelector('a:first-child');
+
+                aLink.classList.remove('active');
+                i.classList.remove('show');
+            })
+        }
+
+        this.classList.toggle('active');
+        item.classList.toggle('show');
+    })
+})
