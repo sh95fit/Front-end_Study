d0d93aa4bc2789ed5cce3ed8c84a94c1e1a8b6c0
sh95fit <h95fit@gmail.com>
2023-03-07 15:55:36 +0900

    Vuejs - User Snippets 등록 & 데이터바인딩 HTML 적용 실습(v-html)

--- Vuejs3_Study/project/src/App.vue
+++ Vuejs3_Study/project/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/databinding/string">String</router-link> |
+    <router-link to="/databinding/html">HTML</router-link>
   </nav>
   <router-view/>
 </template>
--- Vuejs3_Study/project/src/router/index.js
+++ Vuejs3_Study/project/src/router/index.js
@@ -20,6 +20,12 @@ const routes = [
     name: 'DataBindingStringView',
 
     component: () => import(/* webpackChunkName: "databinding", webpackPrefetch: true */ '../views/1_databinding/DataBindingStringView.vue')
+  },
+  {
+    path: '/databinding/html',
+    name: 'DataBindingHtmlView',
+
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch: true */ '../views/1_databinding/DataBindingHtmlView.vue')
   }
 ]
 
