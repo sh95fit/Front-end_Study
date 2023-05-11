b5d496477c2f176ec4e9c65e0a65e9b05e878bde
sh95fit <h95fit@gmail.com>
2023-05-11 17:33:57 +0900

    React - 컴포넌트 기초

--- React_Study/test/src/App.js
+++ React_Study/test/src/App.js
@@ -1,10 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 컴포넌트 - 사용자정의함수
+function Header(){
+  return <header>
+    <h1><a href="/">WEB</a></h1>
+  </header>
+}
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+
 function App() {
   return (
     <div className="App">
-      Hello React !!
+      {/* <header>
+        <h1><a href="/">WEB</a></h1>
+      </header> */}
+
+      {/* <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav> */}
+      {/* <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article> */}
+
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
