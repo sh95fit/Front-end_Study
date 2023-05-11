3785f989a3e1800ffa4a861f0cc439596f18cf51
sh95fit <h95fit@gmail.com>
2023-05-11 17:51:06 +0900

    React - props 기초

--- React_Study/test/src/App.js
+++ React_Study/test/src/App.js
@@ -2,30 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 // 컴포넌트 - 사용자정의함수
-function Header(){
+function Header(props){
+  // console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/">{ props.title }</a></h1>
   </header>
 }
 
-function Nav(){
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>
 }
 
-function Article(){
+function Article(props){
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
+
   return (
     <div className="App">
       {/* <header>
@@ -44,9 +55,9 @@ function App() {
         Hello, WEB
       </article> */}
 
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, REACT"></Article>
     </div>
   );
 }
