fd493cd75afc32befeb6c49d9861337aa7745a5a
sh95fit <h95fit@gmail.com>
2023-05-15 14:22:04 +0900

    Javascript - 비교, 논리 연산자, if-else 조건문

---
+++ JS_Study/연산자.js
@@ -0,0 +1,35 @@
+let num = 10;
+
+num *= 5;
+console.log(num);
+num -= 5;
+console.log(num);
+num %= 7;
+console.log(num);
+num /= 5;
+console.log(num);
+num++;
+console.log(num);
+
+
+
+// 비교연산자
+// '==' : 값만 비교
+// '===' : 타입까지 같은지 비교
+
+
+
+//if-else 구문
+let age = 29;
+
+if(age > 19){
+  console.log('old');
+} else if(age === 29) {
+  console.log('test');
+} else {
+  console.log('young');
+}
+
+
+// 논리 연산자
+// || (OR), && (AND), ! (NOT)
