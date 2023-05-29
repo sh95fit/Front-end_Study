8be5cfe295e5136ba22c8833bd970c8e0e9c0d01
h95fit <h95fit@gmail.com>
2023-05-29 23:58:36 +0900

    ReactMUI - 로그인 샘플 페이지 클론 코딩

---
+++ React_Study/React_MUI/mui-signin/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container'
+
+function App() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop:8,
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center'
+        }}
+      >
+        <Avatar sx={{m:1, bgcolor:'secondary.main'}}>
+          <LockOutlinedIcon/>
+        </Avatar>
+        <Typography component="h1" variant="h5" sx={{mb:3}}>
+          Sign in
+        </Typography>
+        <TextField 
+          margin='normal'
+          label="Email Address" 
+          required 
+          fullWidth 
+          name="email" 
+          autoComplete='E-mail'
+          autoFocus />
+        <TextField 
+          margin='normal'
+          label="Password" 
+          type="password" 
+          required 
+          fullWidth 
+          name="password"
+          autoComplete='current-password' />
+        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me"/>
+        <Button type="submit" fullWidth variant='contained' sx={{mt:3, mb:2}}>Sign in</Button>
+        <Grid container>
+          <Grid item xs>
+            <Link>Forgot password?</Link>
+          </Grid>
+          <Grid item>
+            <Link>Sign Up</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
