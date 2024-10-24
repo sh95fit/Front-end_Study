1b3a564f9ac8cef3dc118d95910aa3732c0acde2
sh95fit <h95fit@gmail.com>
2024-10-24 18:11:57 +0900

    Ag-grid 검색 기능(Quick Filter) 구현

--- React_AGgird/react-aggrid/src/components/Table.jsx
+++ React_AGgird/react-aggrid/src/components/Table.jsx
@@ -63,6 +63,14 @@ const Table = () => {
     // gridApi.setColumnsVisible(["body","email"], hideColumn)
     setHideColumn(!hideColumn)
     gridApi.sizeColumnsToFit()
+    console.log(gridApi)
+  }
+
+  const onFilterTextChange = (e) => {
+    gridApi.setGridOption(
+      "quickFilterText",
+      e.target.value,
+    )
   }
 
   return (
@@ -70,15 +78,20 @@ const Table = () => {
       <div className='p-4 text-4xl text-center'>
         <h1>React-AgGrid</h1>
       </div>
-      <div>
-        <button className={'bg-blue-500 rounded-md p-2 m-2 text-white font-bold shadow-lg shadow-blue-500/50'} onClick={()=>{onExportClick()}}>Export</button>
-        <select className='p-2 ml-5 font-bold border-2 border-neutral-500 text-neutral-500' onChange={(e) => onPaginationChange(e.target.value)}>
-          <option value="10">10</option>
-          <option value="25">25</option>
-          <option value="50">50</option>
-          <option value="100">100</option>
-        </select>
-        <button className="p-2 m-2 ml-8 font-bold text-white bg-red-500 rounded-md shadow-lg shadow-red-500/50" onClick={showColumn}>Show Body</button>
+      <div className='flex flex-row items-center justify-between bg-neutral-100'>
+        <div>
+          <button className={'bg-blue-500 rounded-md p-2 m-2 text-white font-bold shadow-lg shadow-blue-500/50'} onClick={()=>{onExportClick()}}>Export</button>
+          <select className='p-2 ml-5 font-bold border-2 border-neutral-500 text-neutral-500' onChange={(e) => onPaginationChange(e.target.value)}>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+          <button className="p-2 m-2 ml-8 font-bold text-white bg-red-500 rounded-md shadow-lg shadow-red-500/50" onClick={showColumn}>Show Body</button>
+        </div>
+        <div className='flex-grow p-2 ml-10'>
+          <input className='w-full p-2 border-2 border-green-500 rounded-2xl focus:outline-none focus:border-none focus:ring focus:ring-green-200' type="search" placeholder='Search somethings...' onChange={onFilterTextChange}/>
+        </div>
       </div>
       <div
         className={"ag-theme-quartz"}
@@ -96,6 +109,7 @@ const Table = () => {
             enableBrowserTooltips={true}
             tooltipShowDelay={{tooltipShowDelay:2}}
             pagination={true}
+            paginationPageSizeSelector={[18, 10, 20, 50, 100]}
             paginationPageSize={18}   // 페이지네이션 Row 수 직접 지정
             // paginationAutoPageSize={true}   // 지정된 height에 맞춰 페이지네이션 Row 자동 지정
         />
